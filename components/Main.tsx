02aea95c6f685367dc6e1ae63d71dc7929a44ace
JakeAum <jacobauman00@gmail.com>
2024-09-29 18:42:45 -0600

    chore: build error

--- components/Main.tsx
+++ components/Main.tsx
@@ -36,7 +36,7 @@ const Main = () => {
   };
 
   const debounce = (func: (...args: any[]) => void, delay: number) => {
-    let timeoutId: NodeJS.Timeout;
+    let timeoutId: ReturnType<typeof setTimeout>;
     return (...args: any[]) => {
       clearTimeout(timeoutId);
       timeoutId = setTimeout(() => func(...args), delay);
