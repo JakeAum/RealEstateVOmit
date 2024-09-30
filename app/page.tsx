44a05f0b3e86e1353e80447a4127c3fd2301f343
JakeAum <jacobauman00@gmail.com>
2024-09-29 19:43:13 -0600

    fix, dummy

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import { Suspense } from "react";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import FAQ from "@/components/FAQ";
@@ -6,10 +7,12 @@ import CTA from "@/components/CTA";
 export default function Home() {
   return (
     <>
-      
+      <Suspense fallback={<div>Loading search...</div>}>
       <div style={{ position: 'relative', zIndex: 1000 }}>
           <Header />
         </div>
+      </Suspense>
+      <main>
         <CTA/>
        
         {/* <Main /> */}
@@ -17,6 +20,7 @@ export default function Home() {
         
         {/* <WithWithout/> */}
         <FAQ />
+      </main>
       <Footer />
     </>
   );
